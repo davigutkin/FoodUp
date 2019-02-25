6019f72898cf37821ca3f9ae983da6fee526ca53
davigutkin <davigutkin@gmail.com>
2019-02-24 20:19:59 -0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+//create new request variable
+var request = new XMLHttpRequest();
+var city_id = 1
+//Open new connection
+
+request.open('GET', 'https://developers.zomato.com/api/v2.1/cuisines?city_id='+city_id);
+request.setRequestHeader('Api-Key', 'd150c843411d21a3a23e0ea967319814');
+
+var data = this.response;
+console.log(this.response);
